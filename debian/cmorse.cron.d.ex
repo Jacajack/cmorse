4ce479aa6e21ab923e0e2ee77fd0ecce621f3ba8
Jacajack <Jacajack@wp.pl>
2016-05-13 23:19:57 +0200

    Update packaging information

--- debian/cmorse.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the cmorse package
-#
-0 4	* * *	root	[ -x /usr/bin/cmorse_maintenance ] && /usr/bin/cmorse_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(cmorse):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="cmorse" command="/usr/bin/cmorse"
