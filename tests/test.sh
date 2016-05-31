f8abd50b6d6e66f1b52a026f394fa8ee220d20b1
Jacek Wieczorek <mrjjot@gmail.com>
2016-05-31 15:41:04 +0200

    New fancy test procedure

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,20 @@
+touch testlog.log
 ../bin/cmorse -i test.txt -o morse
 ../bin/cmorse -d -i morse -o text
 if ! cmp test.txt text
 then
-echo "Files are not the same!"
+echo "Files are not the same!" >> testlog.log
 fi
-rm -f morse text
+#Before deleting files log their content
+cat test.txt >> testlog.log
+echo "<<<<>>>>" >> testlog.log
+cat morse >> testlog.log
+echo "<<<<>>>>" >> testlog.log
+cat text >> testlog.log
+
+if pastebinit -h
+then
+	pastebinit -i testlog.log
+fi
+
+rm -f morse text testlog.txt
