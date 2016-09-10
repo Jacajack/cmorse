7756a97375a12132734eaa11dc092fabdd2466f2
Jacek Wieczorek <mrjjot@gmail.com>
2016-09-10 18:42:35 +0200

    Add copyright information

--- src/cmorse.c
+++ src/cmorse.c
@@ -1,5 +1,22 @@
-/* This is simple text-Morse's alphabet converter writen in C */
-/* By Jacajack (Jacek Wieczorek) <mrjjot@gmail.com> 2016 */
+/*
+	cmorse - simple text to Morse code converter
+    Copyright (C) 2016  Jacek Wieczorek <mrjjot@gmail.com>
+
+	This file is part of cmorse.
+
+    Cmorse is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cmorse is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "cmorse.h"
 
