5b7b5d43068a57a1325895bb77241a09a36f6d8c
Jacek Wieczorek <mrjjot@gmail.com>
2016-09-10 18:42:35 +0200

    Add copyright information

--- src/cmorse.h
+++ src/cmorse.h
@@ -1,3 +1,23 @@
+/*
+	cmorse - simple text to Morse code converter
+    Copyright (C) 2016  Jacek Wieczorek <mrjjot@gmail.com>
+
+	This file is part of cmorse.
+
+    Cmorse is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cmorse is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <error.h>
 #include <stdlib.h>
